b9495db14eed126a573842b05ac4443595fdf8db
Ren <renrror@gmail.com>
2024-10-23 14:03:31 +0700

    feat: add index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import { createArrayValidator } from './array';
+import { createBooleanValidator } from './boolean';
+import { createNumberValidator } from './number';
+import { createObjectValidator } from './object';
+import { createStringValidator } from './string';
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('dev only output');
   }
   return a + b;
+};
+
+export const avery = {
+  array: createArrayValidator,
+  boolean: createBooleanValidator,
+  number: createNumberValidator,
+  object: createObjectValidator,
+  string: createStringValidator,
 };
