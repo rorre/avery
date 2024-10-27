5e5cdfe77aae2e8ec855d06d177a0f9d99712216
Rendy Arya Kemal <renrror@gmail.com>
2024-10-27 22:20:08 +0700

    fix: validate every member and aggregate

--- src/array.ts
+++ src/array.ts
@@ -1,4 +1,4 @@
-import { Ok, Result } from './monad/result';
+import { Err, Ok, Result } from './monad/result';
 import { baseValidator, nullValidator, Validator } from './validator';
 
 function _createValidator<T>(
@@ -20,12 +20,27 @@ function _createValidator<T>(
 
 export function createArrayValidator<T>(baseValidator: Validator<T, string[]>) {
   return _createValidator<T>((data) =>
-    data.reduce(
-      (a, b) =>
-        a.bind((partial) =>
-          baseValidator.validate(b).fmap((v) => [...partial, v])
-        ),
-      Ok([]) as Result<T[], string[]>
-    )
+    data
+      .map((value) => baseValidator.validate(value))
+      .reduce(
+        (a, b, idx) =>
+          a
+            .bind(
+              (partial) =>
+                (b.isOk() ? Ok([...partial, b.unwrap()]) : Err([])) as Result<
+                  T[],
+                  string[]
+                >
+            )
+            .fmapErr((err) =>
+              b.isErr()
+                ? [
+                    ...err,
+                    ...b.unwrapErr().map((error) => `[${idx}]: ${error}`),
+                  ]
+                : err
+            ),
+        Ok([]) as Result<T[], string[]>
+      )
   );
 }
