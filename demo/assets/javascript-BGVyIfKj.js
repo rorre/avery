b12071350b95da9d7ef4a6b34653f05170ac9ba9
Ren <renrror@gmail.com>
2024-11-28 10:27:17 +0700

    Update documentation

--- demo/assets/javascript-BGVyIfKj.js
+++ demo/assets/javascript-BGVyIfKj.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-C2gC2kVX.js";import"./index-CHNIR1Em.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-1f6eEQnS.js";import"./index-CyMcV9Y9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
