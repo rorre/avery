8084dcc840df4a9420bb2ab1bb3b0807049f9f13
Ren <renrror@gmail.com>
2024-11-28 10:34:11 +0700

    Update documentation

--- demo/assets/tsMode-B64tP0U1.js
+++ demo/assets/tsMode-B64tP0U1.js
@@ -1,4 +1,4 @@
-import{t as O,m as I}from"./index-CyMcV9Y9.js";/*!-----------------------------------------------------------------------------
+import{t as O,m as I}from"./index-COYbvEcd.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
